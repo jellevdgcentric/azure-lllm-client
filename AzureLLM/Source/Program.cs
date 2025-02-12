e0afe5ae2ae8753eba2638f7b612d51bc2047fc7
Jelle van der Gulik <jellevdg3@gmail.com>
2025-02-12 11:53:21 +0100

    Use the Azure SDK clients in the code for the demo

--- AzureLLM/Source/Program.cs
+++ AzureLLM/Source/Program.cs
@@ -4,6 +4,8 @@ using AzureLLM.Source;
 using Microsoft.Extensions.Configuration;
 using AzureLLM.Source.SDK;
 using AzureLLM.Source.HTTP;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MyApp
 {
@@ -29,54 +31,22 @@ namespace MyApp
             string searchVectorDatabaseName = azureSearchConfig["VectorDatabaseName"];
             string searchApiKey = azureSearchConfig["ApiKey"];
 
-            IChatClient azureChatClient = new AzureHTTPChatClient(llmEndpoint, llmDeploymentName, llmApiKey);
+            IChatClient azureChatClient = new AzureChatClient(llmEndpoint, llmDeploymentName, llmApiKey);
             azureChatClient.UseHistory(true);
-			ISemanticSearcher semanticSearcher = new AzureHTTPSemanticSearcher(searchEndpoint, searchVectorDatabaseName, searchApiKey);
+            ISemanticSearcher semanticSearcher = new AzureSemanticSearcher(searchEndpoint, searchVectorDatabaseName, searchApiKey);
 
-            //await GPTAssistent(azureChatClient);
+            await GPTAssistant(azureChatClient);
 
-			await RacingAssistent(azureChatClient, semanticSearcher);
-			//await GrandmaAndGransonWhatsappConversation(llmEndpoint, llmDeploymentName, llmApiKey);
-		}
+            //await RacingAssistant(azureChatClient, semanticSearcher);
+            //await GrandmaAndGrandsonWhatsappConversation(llmEndpoint, llmDeploymentName, llmApiKey);
+        }
 
-		private static async Task GPTAssistent(IChatClient azureChatClient)
-		{
-			Console.ForegroundColor = ConsoleColor.DarkBlue;
-			Console.WriteLine("Hello I am a GPT assistent. Can I help you?");
-
-			azureChatClient.UseHistory(true);
-
-			while (true)
-			{
-				Console.ForegroundColor = ConsoleColor.Black;
-				Console.Write("You: ");
-				string? userInput = Console.ReadLine();
-
-				if (userInput == null || userInput.Trim().ToLower() == "exit")
-				{
-					break;
-				}
-
-				if (userInput.Trim() == string.Empty)
-				{
-					continue;
-				}
-
-				if (userInput.Trim().ToLower() == "clear")
-				{
-					Console.Clear();
-					continue;
-				}
-
-				Console.ForegroundColor = ConsoleColor.DarkBlue;
-				await Prompt(azureChatClient, userInput);
-			}
-		}
-
-		private static async Task RacingAssistent(IChatClient azureChatClient, ISemanticSearcher semanticSearcher)
+        private static async Task GPTAssistant(IChatClient azureChatClient)
         {
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("Hello I am your iRacing assistent for today. Do you have any questions?");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Hello I am a GPT assistant. Can I help you?");
+
+            azureChatClient.UseHistory(true);
 
             while (true)
             {
@@ -100,48 +70,12 @@ namespace MyApp
                     continue;
                 }
 
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
-                await PromptWithSemanticSearch(azureChatClient, semanticSearcher, userInput);
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                await Prompt(azureChatClient, "You are a GPT assistant and try to answer the user its questions.", userInput);
             }
         }
 
-        private static async Task GrandmaAndGransonWhatsappConversation(Uri llmEndpoint, string llmDeploymentName, string llmApiKey)
-        {
-            IChatClient grandmaClient = new AzureChatClient(llmEndpoint, llmDeploymentName, llmApiKey);
-            IChatClient grandsonClient = new AzureChatClient(llmEndpoint, llmDeploymentName, llmApiKey);
-
-			string systemPromptGrandma = "You are an 85 year old grandma called Gertrude, and you're talking to your grandson called Charles. Do not end or lead to an ending of the conversation.";
-			string systemPromptGrandson = "You are a 15 year old grandson called Charles, and you're talking to your grandma called Gertrude. Do not end or lead to an ending of the conversation.";
-
-			string messageFromGrandma = "Hi Charles, how are you today?";
-
-			grandmaClient.UseHistory(true, new List<(string, string)> { ("user", messageFromGrandma) });
-			grandsonClient.UseHistory(true);
-
-            Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            Console.WriteLine("Gertrude: " + messageFromGrandma);
-            string responseFromGrandson = await grandsonClient.PromptAsync(systemPromptGrandson, messageFromGrandma);
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("Charles: " + responseFromGrandson);
-
-			while (true)
-            {
-                Console.ReadLine();
-
-                string responseFromGrandma = await grandmaClient.PromptAsync(systemPromptGrandma, responseFromGrandson);
-
-                Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                Console.WriteLine("Gertrude: " + responseFromGrandma);
-
-                string responseFromGrandsonNew = await grandsonClient.PromptAsync(systemPromptGrandson, responseFromGrandma);
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("Charles: " + responseFromGrandsonNew);
-
-                responseFromGrandson = responseFromGrandsonNew;
-            }
-        }
-
-        private static async Task<string> Prompt(IChatClient azureChatClient, string userInput)
+        private static async Task<string> Prompt(IChatClient azureChatClient, string systemPrompt, string userInput)
         {
             if (UseStreaming)
             {
@@ -159,7 +93,7 @@ namespace MyApp
             }
         }
 
-        private static async Task PromptWithSemanticSearch(IChatClient azureChatClient, ISemanticSearcher semanticSearcher, string userInput)
+        private static async Task PromptWithSemanticSearch(IChatClient azureChatClient, ISemanticSearcher semanticSearcher, string systemPrompt, string userInput)
         {
             List<string> semanticSearchResult = await semanticSearcher.SearchAsync(userInput);
             string formattedSearchResults = "### Retrieved Information:\n" +
@@ -168,11 +102,8 @@ namespace MyApp
                 userInput +
                 "\n\nBased on the above information, provide a concise and relevant answer to the user's query. Respond in the language based on the user query.";
 
-            //Console.ForegroundColor = ConsoleColor.DarkRed;
-            //Console.WriteLine(formattedSearchResults);
-
-			Console.ForegroundColor = ConsoleColor.DarkGreen;
-			if (UseStreaming)
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            if (UseStreaming)
             {
                 Console.Write("AI: ");
                 await azureChatClient.PromptStreamingAsync(string.Empty, formattedSearchResults, (response) =>
@@ -188,5 +119,73 @@ namespace MyApp
                 Console.WriteLine(response);
             }
         }
-    }
+
+		private static async Task RacingAssistant(IChatClient azureChatClient, ISemanticSearcher semanticSearcher)
+		{
+			Console.ForegroundColor = ConsoleColor.DarkGreen;
+			Console.WriteLine("Hello I am your iRacing assistant for today. Do you have any questions?");
+
+			while (true)
+			{
+				Console.ForegroundColor = ConsoleColor.Black;
+				Console.Write("You: ");
+				string? userInput = Console.ReadLine();
+
+				if (userInput == null || userInput.Trim().ToLower() == "exit")
+				{
+					break;
+				}
+
+				if (userInput.Trim() == string.Empty)
+				{
+					continue;
+				}
+
+				if (userInput.Trim().ToLower() == "clear")
+				{
+					Console.Clear();
+					continue;
+				}
+
+				Console.ForegroundColor = ConsoleColor.DarkGreen;
+				await PromptWithSemanticSearch(azureChatClient, semanticSearcher, "You are an iRacing virtual assistant and try to help the user answer questions.", userInput);
+			}
+		}
+
+		private static async Task GrandmaAndGrandsonWhatsappConversation(Uri llmEndpoint, string llmDeploymentName, string llmApiKey)
+		{
+			IChatClient grandmaClient = new AzureChatClient(llmEndpoint, llmDeploymentName, llmApiKey);
+			IChatClient grandsonClient = new AzureChatClient(llmEndpoint, llmDeploymentName, llmApiKey);
+
+			string systemPromptGrandma = "You are an 85 year old grandma called Gertrude, and you're talking to your grandson called Charles. Do not end or lead to an ending of the conversation.";
+			string systemPromptGrandson = "You are a 15 year old grandson called Charles, and you're talking to your grandma called Gertrude. Do not end or lead to an ending of the conversation.";
+
+			string messageFromGrandma = "Hi Charles, how are you today?";
+
+			grandmaClient.UseHistory(true, new List<(string, string)> { ("user", messageFromGrandma) });
+			grandsonClient.UseHistory(true);
+
+			Console.ForegroundColor = ConsoleColor.DarkMagenta;
+			Console.WriteLine("Gertrude: " + messageFromGrandma);
+			string responseFromGrandson = await grandsonClient.PromptAsync(systemPromptGrandson, messageFromGrandma);
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.WriteLine("Charles: " + responseFromGrandson);
+
+			while (true)
+			{
+				Console.ReadLine();
+
+				string responseFromGrandma = await grandmaClient.PromptAsync(systemPromptGrandma, responseFromGrandson);
+
+				Console.ForegroundColor = ConsoleColor.DarkMagenta;
+				Console.WriteLine("Gertrude: " + responseFromGrandma);
+
+				string responseFromGrandsonNew = await grandsonClient.PromptAsync(systemPromptGrandson, responseFromGrandma);
+				Console.ForegroundColor = ConsoleColor.DarkRed;
+				Console.WriteLine("Charles: " + responseFromGrandsonNew);
+
+				responseFromGrandson = responseFromGrandsonNew;
+			}
+		}
+	}
 }
